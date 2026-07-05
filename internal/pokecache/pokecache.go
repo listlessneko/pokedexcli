fb530f6a6f266e03ace35958db17bf0022834bf2
kedarui <98135875+listlessneko@users.noreply.github.com>
2026-07-05 12:33:04 -0700

    Added func to create new cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -10,6 +10,13 @@ type cacheEntry struct {
 }
 
 type Cache struct {
-	cache map[string]cachedEntry
+	cache map[string]cacheEntry
 	mu sync.Mutex
+}
+
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{
+		cache: make(map[string]cacheEntry),
+	}
+	return c
 }
