c85eef8581424eb06762009734981c785636a6d7
kedarui <98135875+listlessneko@users.noreply.github.com>
2026-07-05 13:20:43 -0700

    Getting get cache entries ready

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -31,3 +31,14 @@ func (c *Cache) Add(key string, val []byte) {
 	}
 	c.cache[key] = ce
 }
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	entry, ok := c.cache[key] 
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
+}
