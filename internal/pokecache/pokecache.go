3fbeeef4d335ed4b1bb8779d51d6ad7ce369edac
kedarui <98135875+listlessneko@users.noreply.github.com>
2026-07-05 13:43:49 -0700

    Reaping the cache for those whose time is up

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -18,6 +18,7 @@ func NewCache(interval time.Duration) *Cache {
 	c := &Cache{
 		cache: make(map[string]cacheEntry),
 	}
+	go c.reapLoop(interval)
 	return c
 }
 
@@ -41,4 +42,18 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 		return nil, false
 	}
 	return entry.val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+
+	for range ticker.C {
+		c.mu.Lock()
+		for key, entry := range c.cache {
+			if time.Since(entry.createdAt) > interval {
+				delete(c.cache, key)
+			}
+		}
+		c.mu.Unlock()
+	}
 }
