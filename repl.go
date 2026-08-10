858108f29f9d7b9dee541df78fcbb712a8f3cef8
kedarui <98135875+listlessneko@users.noreply.github.com>
2026-08-09 21:41:52 -0700

    Extract select/load profile and loop handling into their own functions

--- repl.go
+++ repl.go
@@ -9,23 +9,14 @@ import (
 	"time"
 )
 
-func startRepl() {
-	app := &app{}
-	app.config = &config{
-		Prompt: "Pokedex > ",
-		Caught: make(map[string]Pokemon),
-	}
-	app.cache = &cache{
-		Cache: pokecache.NewCache(60 * time.Second),
-	}
-
+func selectProfile(app *app) {
 	index, err := loadProfilesIndex()
 	if err != nil {
 		os.Stderr.Write([]byte(err.Error()))
 	}
 	if len(index) > 0 {
-		keys:= justTheKeys(index)
-		prompts:= sortSlices(keys, asc)
+		keys := justTheKeys(index)
+		prompts := sortSlices(keys, asc)
 		prompts = append(prompts, "[New Save]")
 		selectedPrompt, err := selectPrompt(prompts)
 		if err != nil {
@@ -34,10 +25,12 @@ func startRepl() {
 
 		saveFilename := index[selectedPrompt]
 		if saveFilename != "" {
-			app.config.SaveFile = "saves/" + saveFilename + ".json"
+			app.config.SaveFile = save_ify(saveFilename)
 		}
 	}
+}
 
+func loadProfile(app *app) {
 	data, err := os.ReadFile(app.config.SaveFile)
 	if !os.IsNotExist(err) {
 		if err == nil {
@@ -47,16 +40,18 @@ func startRepl() {
 			os.Stderr.Write([]byte(err.Error()))
 		}
 	}
+}
 
+func beginTheLoop(app *app) {
 	var history []string
-	autocomplete := newTrie()
+	commandsTrie := newTrie()
 	commands := getCommands()
 	for command, _ := range commands {
-		autocomplete.add(command)
+		commandsTrie.add(command)
 	}
 
 	for {
-		line, err := readLine(app.config.Prompt, history, autocomplete)
+		line, err := readLine(app.config.Prompt, history, commandsTrie)
 		if errors.Is(err, io.EOF) {
 			os.Stdout.Write([]byte{newLine})
 			break
@@ -74,15 +69,32 @@ func startRepl() {
 		history = append(history, line)
 
 		command, exists := commands[userInput[0]]
-		if exists {
-			app.writer = os.Stdout
-			app.args = userInput[1:]
-			err := command.callback(app)
-			if err != nil {
-				os.Stderr.Write([]byte(err.Error() + "\n"))
-			}
-		} else {
+		if !exists {
 			os.Stdout.Write([]byte("unknown command\n"))
+			continue
 		}
+		app.writer = os.Stdout
+		app.args = userInput[1:]
+		err = command.callback(app)
+		if err != nil {
+			os.Stderr.Write([]byte(err.Error() + "\n"))
+			continue
+		} 
+		continue
 	}
+}
+
+func startRepl() {
+	app := &app{}
+	app.config = &config{
+		Prompt: "Pokedex > ",
+		Caught: make(map[string]Pokemon),
+	}
+	app.cache = &cache{
+		Cache: pokecache.NewCache(60 * time.Second),
+	}
+
+	selectProfile(app)
+	loadProfile(app)
+	beginTheLoop(app)
 }
