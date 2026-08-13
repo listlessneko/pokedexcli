f6849dd34a4d1d675cefb2389a26a12cb0e44be4
kedarui <98135875+listlessneko@users.noreply.github.com>
2026-08-13 07:27:49 -0700

    Defer close yard

--- repl.go
+++ repl.go
@@ -94,7 +94,12 @@ func startRepl() {
 		Cache: pokecache.NewCache(60 * time.Second),
 	}
 
-	openYard()
+	yard, err := openYard()
+	if err != nil {
+		return
+	}
+
+	defer yard.Close()
 	selectProfile(app)
 	loadProfile(app)
 	beginTheLoop(app)
