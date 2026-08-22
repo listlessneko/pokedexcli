c470d47f73f6add7124e670ff0107509716cd3b6
kedarui <98135875+listlessneko@users.noreply.github.com>
2026-08-22 10:09:12 -0700

    Extract getting pokemon and species data into their own functions in pokeapi

--- pokeapi.go
+++ pokeapi.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -26,3 +28,48 @@ func fetch(url string, app *appState) ([]byte, error) {
 	logger.Bark("fetched")
 	return b, nil
 }
+
+func getPokemonSpecies(app *appState) (PokemonSpecies, error) {
+	url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon-species/%s", app.args[0])
+
+	var species PokemonSpecies
+
+	b, err := fetch(url, app)
+	if err != nil {
+		return species, err
+	}
+
+	if string(b) == "Not Found" {
+		fmt.Fprintln(app.writer, "Please provide a valid Pokemon.")
+		return species, nil
+	}
+
+	err = json.Unmarshal(b, &species)
+	if err != nil {
+		return species, err
+	}
+	return species, nil
+}
+
+func getPokemon(app *appState) (Pokemon, error) {
+	url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", app.args[0])
+
+	var pokemon Pokemon
+
+	b, err := fetch(url, app)
+	if err != nil {
+		return pokemon, err
+	}
+
+	if string(b) == "Not Found" {
+		fmt.Fprintln(app.writer, "Please provide a valid Pokemon.")
+		return pokemon, nil
+	}
+
+	err = json.Unmarshal(b, &pokemon)
+	if err != nil {
+		return pokemon, err
+	}
+	return pokemon, nil
+}
+
